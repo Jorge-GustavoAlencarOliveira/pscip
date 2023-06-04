d9d11df4ba7a4bc34fcac843a3da7f4c46e353c8
Jorge-GustavoAlencarOliveira <jorgegustavo500@gmail.com>
2023-06-04 20:56:13 -0300

    1

--- src/pages/informations/index.tsx
+++ src/pages/informations/index.tsx
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 // import Head from 'next/head'
 // import { DataStorage } from '../../../dataContext'
 // import styles from '../home.module.css'
